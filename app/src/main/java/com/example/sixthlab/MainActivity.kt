c64d1c0c76bc93def6dce72cf09b2548eddba185
tematrkv <tatarnikov.stud@mail.ru>
2024-10-29 11:43:58 +0700

    fifth commit (MainActivity is the host)

--- app/src/main/java/com/example/sixthlab/MainActivity.kt
+++ app/src/main/java/com/example/sixthlab/MainActivity.kt
@@ -11,6 +11,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val currentFragment =
+            supportFragmentManager.findFragmentById(R.id.fragment_container)
+        if (currentFragment == null) {
+            val fragment = CrimeFragment()
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.fragment_container, fragment)
+                .commit()
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.fragment_container)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
