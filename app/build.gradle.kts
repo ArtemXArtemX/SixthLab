59d9bfb705d9ad60c8f4fc76298e167ca5fd541f
tematrkv <tatarnikov.stud@mail.ru>
2024-10-29 21:40:18 +0700

    6.2 start commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("org.jetbrains.kotlin.kapt")
 }
 
 android {
@@ -36,7 +37,6 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
@@ -48,4 +48,6 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.5.1")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.1")
     implementation("androidx.recyclerview:recyclerview:1.0.0")
+    implementation("androidx.room:room-runtime:2.1.0-alpha04")
+    kapt("androidx.room:room-compiler:2.1.0-alpha04")
 }
